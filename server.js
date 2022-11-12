80c59c114779092801d1aac9c4b6d77d78c40620
Shobhit Nautiyal <88445550+shobhit1812@users.noreply.github.com>
2022-11-12 22:08:37 +0530

    Update server.js
    add  process.env.PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 require("dotenv").config()
-const PORT = 5000
+const PORT = process.env.PORT
 
 const cors = require("cors")
 
